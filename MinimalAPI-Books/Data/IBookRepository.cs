898e97e3d50f7e427196d809b35e2b0cb0899e8b
Max Dahlberg <max.dahlberg1@edu.varberg.se>
2023-08-28 12:10:37 +0200

    Added IBookrepository and its implementation

---
+++ MinimalAPI-Books/Data/IBookRepository.cs
@@ -0,0 +1,14 @@
+﻿using MinimalAPI_Books.Models;
+
+namespace MinimalAPI_Books.Data
+{
+    public interface IBookRepository
+    {
+        Task<Book> GetByIdAsync(int id);
+        Task<IEnumerable<Book>> GetAllAsync();
+        Task AddAsync(Book book);
+        Task UpdateAsync(Book book);
+        Task DeleteAsync(int id);
+    }
+
+}
