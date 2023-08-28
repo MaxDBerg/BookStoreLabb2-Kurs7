dd289a1577a478f416e20eb3240affb5aa047af1
Max Dahlberg <max.dahlberg1@edu.varberg.se>
2023-08-28 13:56:38 +0200

    Seperated GenreSeed and LanguageSeed

--- MinimalAPI-Books/Data/GenreSeed.cs
+++ MinimalAPI-Books/Data/GenreSeed.cs
@@ -4,7 +4,7 @@
     using Microsoft.EntityFrameworkCore.Metadata.Builders;
     using MinimalAPI_Books.Models;
 
-    public class GenreLanguageSeed : IEntityTypeConfiguration<Genre>
+    public class GenreSeed : IEntityTypeConfiguration<Genre>
     {
         public void Configure(EntityTypeBuilder<Genre> builder)
         {
@@ -27,23 +27,4 @@
             ); ;
         }
     }
-
-    public class LanguageSeed : IEntityTypeConfiguration<Language>
-    {
-        public void Configure(EntityTypeBuilder<Language> builder)
-        {
-            builder.HasData(
-                new Language { Id = 1, Name = "English" },
-                new Language { Id = 2, Name = "Spanish" },
-                new Language { Id = 3, Name = "French" },
-                new Language { Id = 4, Name = "German" },
-                new Language { Id = 5, Name = "Italian" },
-                new Language { Id = 6, Name = "Portuguese" },
-                new Language { Id = 7, Name = "Russian" },
-                new Language { Id = 8, Name = "Swedish" },
-                new Language { Id = 9, Name = "Japanese" }
-            ); ;
-        }
-    }
-
 }
